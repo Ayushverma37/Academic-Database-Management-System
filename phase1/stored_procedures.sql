82fb269b44c2319ddac7abb953f6b035ee2d7689
Keshav Krishna <80466550+k-keshav-k@users.noreply.github.com>
2021-10-19 13:35:55 +0530

    added stored procedure- student registration
    Added stored procedure for student registration
    uses procedure allot_section() to get the allocated section

--- phase1/stored_procedures.sql
+++ phase1/stored_procedures.sql
@@ -1,1 +1,15 @@
--- thsi file contains stored procedures
+-- this file contains stored procedures
+
+-- procedure for student registration in a course
+-- procedures to be implemented: allot_section()
+CREATE OR REPLACE FUNCTION registration_in_course(student_id char(11), course_id char(5), semester INTEGER, year INTEGER)
+RETURNS NULL
+LANGUAGE PLPGSQL
+AS $$
+DECLARE
+section_id INTEGER;
+BEGIN
+    section_id := allot_section();
+    EXECUTE format('INSERT INTO Student_Registration values(%L, %L, %L, %L, %L);', student_id, course_id, semester, year, section_id);
+END;
+$$;
