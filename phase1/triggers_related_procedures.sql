11c52b36b582a7f7843f4037ea89d1c49865e51f
AYUSH VERMA <2019csb1147@iitrpr.ac.in>
2021-10-25 11:29:00 +0530

    Update triggers_related_procedures.sql

--- phase1/triggers_related_procedures.sql
+++ phase1/triggers_related_procedures.sql
@@ -397,13 +397,17 @@ cgpaReq numeric;
 temp_semester INTEGER;
 temp_year INTEGER;
 BEGIN
-    curr_grade:=curr_cgpa(NEW.student_id);
-    select semester into temp_semester from current_sem_and_year;
-    select year into temp_year from current_sem_and_year;
-    -- select cgpa_criterion into cgpaReq from Course_Offering as CO where CO.course_id=NEW.course_id;
-    EXECUTE format ('select cgpa_criterion from %I as CO where CO.course_id=%L;', 'course_offering_'||temp_semester||'_'||temp_year, NEW.course_id) into cgpaReq;
-    IF (cgpaReq IS NOT NULL) AND (curr_grade<cgpaReq) THEN
-      RAISE EXCEPTION 'cgpa of Student is less than cgpa criteria for this course';
+    IF cgpaReq IS NOT NULL THEN
+    BEGIN
+        curr_grade:=curr_cgpa(NEW.student_id);
+        select semester into temp_semester from current_sem_and_year;
+        select year into temp_year from current_sem_and_year;
+        -- select cgpa_criterion into cgpaReq from Course_Offering as CO where CO.course_id=NEW.course_id;
+        EXECUTE format ('select cgpa_criterion from %I as CO where CO.course_id=%L;', 'course_offering_'||temp_semester||'_'||temp_year, NEW.course_id) into cgpaReq;
+        IF (curr_grade<cgpaReq) THEN
+        RAISE EXCEPTION 'cgpa of Student is less than cgpa criteria for this course';
+        END IF;
+    END;
     END IF;
     return NEW;
 END;
