f6ec6eb9ee0f189774231d4c4e9fb548bd9da9c9
AYUSH VERMA <2019csb1147@iitrpr.ac.in>
2021-10-25 23:25:51 +0530

    Update triggers_related_procedures.sql

--- phase1/triggers_related_procedures.sql
+++ phase1/triggers_related_procedures.sql
@@ -114,18 +114,18 @@ temp_semester INTEGER;
 temp_year INTEGER;
 stud_batch INTEGER;
 BEGIN
-    select semester into temp_semester from current_sem_and_year;
-    select year into temp_year from current_sem_and_year;
-    select batch into stud_batch from Student where NEW.student_id=Student.student_id;
-    credits_registered := get_registered_credits_previous_2_semester(NEW.student_id, temp_semester, temp_year);
-    max_credits_allowed := 1.25*credits_registered;
-    credits_in_this_sem := get_credits_registered_in_this_sem(NEW.student_id);
-    select CC.C into credits_for_new_course from Course_Catalog as CC where CC.course_id = NEW.course_id;
     IF stud_batch<temp_year THEN
     BEGIN
+        select semester into temp_semester from current_sem_and_year;
+        select year into temp_year from current_sem_and_year;
+        select batch into stud_batch from Student where NEW.student_id=Student.student_id;
+        credits_registered := get_registered_credits_previous_2_semester(NEW.student_id, temp_semester, temp_year);
+        max_credits_allowed := 1.25*credits_registered;
+        credits_in_this_sem := get_credits_registered_in_this_sem(NEW.student_id);
+        select CC.C into credits_for_new_course from Course_Catalog as CC where CC.course_id = NEW.course_id;
         if credits_for_new_course + credits_in_this_sem > max_credits_allowed then
             --ticket generation function call
-            raise exception 'Credit limit exceeded, ticket generated';
+        raise exception 'Credit limit exceeded, ticket generated';
         end if;
     END;
     END IF;
