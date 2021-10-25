b870a2a5ed705d779ae25426bea492a255b299e2
AYUSH VERMA <2019csb1147@iitrpr.ac.in>
2021-10-25 22:41:13 +0530

    Update triggers_related_procedures.sql

--- phase1/triggers_related_procedures.sql
+++ phase1/triggers_related_procedures.sql
@@ -260,6 +260,13 @@ END;
 $$;
 
 --trigger and procedure to check if student has done course_id_not_eligible course
+create or replace function get_current_course_prohibition(cid char(5), stud_id char(11))
+returns table (course char(5),grade INTEGER)
+LANGUAGE plpgsql AS $$
+begin
+return query EXECUTE format('select course_id, grade from %I as TS where TS.course_id=%L and TS.grade>=4.0;', 'trans_'||stud_id, cid);
+end; $$;
+
 create or replace function get_course_prohibition(cid char(5), stud_id char(11))
 returns table (course char(5),grade INTEGER)
 LANGUAGE plpgsql AS $$
@@ -275,6 +282,9 @@ DECLARE
 course_prohibited char(5);
 BEGIN
     select course_id_Not_Elligible into course_prohibited from Course_Catalog as CC where CC.course_id=NEW.course_id;
+    IF EXISTS (select * from get_current_course_prohibition(NEW.course_id,NEW.student_id)) then
+        RAISE EXCEPTION 'Student has already done same course';
+    END IF;
     IF course_prohibited IS NOT NULL THEN
     BEGIN
         IF EXISTS (select * from get_course_prohibition(course_prohibited,NEW.student_id)) then
