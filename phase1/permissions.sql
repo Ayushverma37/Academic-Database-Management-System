91bd6ff46f9ad3ac8c53d66231d1b9a5380d835f
Keshav Krishna <80466550+k-keshav-k@users.noreply.github.com>
2021-10-27 20:25:26 +0530

    Update permissions.sql

--- phase1/permissions.sql
+++ phase1/permissions.sql
@@ -31,6 +31,8 @@ CREATE OR REPLACE FUNCTION create_new_ins_login()
 RETURNS TRIGGER 
 LANGUAGE PLPGSQL 
 AS $$
+DECLARE
+temp_student_id char(11);
 BEGIN 
     EXECUTE format('CREATE USER %I WITH ENCRYPTED PASSWORD %L;', 'instructor_'||NEW.ins_id, 'abc');
     EXECUTE format('GRANT SELECT ON current_sem_and_year TO %I;',  'instructor_'||NEW.ins_id);
@@ -39,7 +41,11 @@ BEGIN
     EXECUTE format('GRANT SELECT ON instructor TO %I;', 'instructor_'||NEW.ins_id);
     EXECUTE format('GRANT SELECT ON batch_adviser TO %I;', 'instructor_'||NEW.ins_id);
     EXECUTE format('GRANT SELECT ON Course_Catalog TO %I;',  'instructor_'||NEW.ins_id);
-    EXECUTE format('GRANT SELECT ON %I TO %I;', 'ticket_student_'||NEW.student_id, 'instructor_'||NEW.ins_id);
+    --EXECUTE format('GRANT SELECT ON %I TO %I;', 'ticket_student_'||NEW.student_id, 'instructor_'||NEW.ins_id);
+    FOR temp_student_id in select student_id from student LOOP 
+        EXECUTE format('GRANT SELECT ON %I TO %I;', 'ticket_student_'||temp_student_id, 'instructor_'||NEW.ins_id);
+    END LOOP;
+
     EXECUTE format('GRANT ALL ON %I TO %I;', 'ticket_instructor_'||NEW.ins_id, 'instructor_'||NEW.ins_id);
     --EXECUTE format('REVOKE ALL ON %I FROM %I;', 'ticket_batch_adviser_'||NEW.ins_id, 'instructor_'||NEW.ins_id);
     --EXECUTE format('REVOKE ALL ON tickets_dean FROM %I;', 'instructor_'||NEW.ins_id);
@@ -58,6 +64,9 @@ CREATE OR REPLACE FUNCTION create_new_ba_login()
 RETURNS TRIGGER 
 LANGUAGE PLPGSQL 
 AS $$
+DECLARE 
+temp_student_id char(11);
+temp_ins_id integer;
 BEGIN 
     EXECUTE format('CREATE USER %I WITH ENCRYPTED PASSWORD %L;', 'batch_adviser_'||NEW.ins_id||'_'||NEW.batch, 'abc');
     EXECUTE format('GRANT SELECT ON current_sem_and_year TO %I;', 'batch_adviser_'||NEW.ins_id||'_'||NEW.batch);
@@ -66,8 +75,16 @@ BEGIN
     EXECUTE format('GRANT SELECT ON instructor TO %I;', 'batch_adviser_'||NEW.ins_id||'_'||NEW.batch);
     EXECUTE format('GRANT SELECT ON batch_adviser TO %I;', 'batch_adviser_'||NEW.ins_id||'_'||NEW.batch);
     EXECUTE format('GRANT SELECT ON Course_Catalog TO %I;', 'batch_adviser_'||NEW.ins_id||'_'||NEW.batch);
-    EXECUTE format('GRANT SELECT ON %I TO %I;', 'ticket_student_'||NEW.student_id, 'batch_adviser_'||NEW.ins_id||'_'||NEW.batch);
-    EXECUTE format('GRANT SELECT ON %I TO %I;', 'ticket_instructor_'||NEW.ins_id, 'batch_adviser_'||NEW.ins_id||'_'||NEW.batch);
+    -- EXECUTE format('GRANT SELECT ON %I TO %I;', 'ticket_student_'||NEW.student_id, 'batch_adviser_'||NEW.ins_id||'_'||NEW.batch);
+    FOR temp_student_id in select student_id from student LOOP
+        EXECUTE format('GRANT SELECT ON %I TO %I;', 'ticket_student_'||temp_student_id, 'batch_adviser_'||NEW.ins_id||'_'||NEW.batch);
+    END LOOP;
+
+    FOR temp_ins_id in select ins_id from instructor LOOP 
+        EXECUTE format('GRANT SELECT ON %I TO %I;', 'ticket_instructor_'||temp_ins_id, 'batch_adviser_'||NEW.ins_id||'_'||NEW.batch);
+    END LOOP;
+
+    -- EXECUTE format('GRANT SELECT ON %I TO %I;', 'ticket_instructor_'||NEW.ins_id, 'batch_adviser_'||NEW.ins_id||'_'||NEW.batch);
     EXECUTE format('GRANT ALL ON %I TO %I;', 'ticket_batch_adviser_'||NEW.ins_id, 'batch_adviser_'||NEW.ins_id||'_'||NEW.batch);
     --EXECUTE format('REVOKE ALL ON tickets_dean FROM %I;', 'batch_adviser_'||NEW.ins_id||'_'||NEW.batch);
     return NULL;
