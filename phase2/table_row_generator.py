443002114a7d1c37660b3db4212ffff9e59274d1
Keshav Krishna <80466550+k-keshav-k@users.noreply.github.com>
2021-11-09 17:54:11 +0530

    Update table_row_generator.py

--- phase2/table_row_generator.py
+++ phase2/table_row_generator.py
@@ -1,1 +1,22 @@
 # This file creates table entries of all the tables
+import numpy as np
+import random
+import string
+
+
+def random_string(size, chars):
+    return ''.join(random.choice(chars) for x in range(size))
+
+
+data = np.random.permutation(80000)
+chars_name = string.ascii_lowercase
+size_name = 10
+names_list = []
+for y in range(80000):
+    names_list.append(random_string(size_name, chars_name))
+
+f = open('rows.txt', 'w')
+for z in range(80000):
+    f.write('INSERT INTO actor values (' + str(data[z]) + ',' + '\'' + names_list[z] + '\'' + ');\n')
+
+f.close()
