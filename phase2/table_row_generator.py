653c43b5a033fa9b6a854e78921dfd681d3d0cf3
AYUSH VERMA <2019csb1147@iitrpr.ac.in>
2021-11-09 18:17:39 +0530

    Update table_row_generator.py

--- phase2/table_row_generator.py
+++ phase2/table_row_generator.py
@@ -22,3 +22,21 @@ for z in range(80000):
     f.write('INSERT INTO actor values (' + str(data[z]) + ',' + '\'' + names_list[z] + '\'' + ');\n')
 
 f.close()
+
+data_pc=np.random.permutation(80000)
+chars_name_pc = string.ascii_lowercase
+size_name_pc = 10
+names_list_pc = []
+for y in range(80000):
+    names_list_pc.append(random_string(size_name_pc, chars_name_pc))
+
+chars_address_pc = string.ascii_lowercase
+address_list_pc = []
+size_address_pc = 30
+for y in range(80000):
+    address_list_pc.append(random_string(size_address_pc, chars_address_pc))
+f_pc = open('production.txt', 'w')
+for z in range(80000):
+    f_pc.write('INSERT INTO production_company values (' + str(data_pc[z]) + ',' + '\'' + names_list_pc[z] + '\''+  ',' + '\'' + address_list_pc[z] + '\'' + ');\n')
+
+f_pc.close()
