499ed66630a51ba8b95ad2db4982f82b630280f4
Keshav Krishna <80466550+k-keshav-k@users.noreply.github.com>
2021-11-12 20:48:11 +0530

    Update table_row_generator.py

--- phase2/table_row_generator.py
+++ phase2/table_row_generator.py
@@ -17,10 +17,10 @@ names_list = []
 for y in range(300000):
     names_list.append(random_string(size_name, chars_name))
 
-f = open('actor.txt', 'w')
+f = open('actor.csv', 'w')
 for z in range(300000):
     # f.write('INSERT INTO actor values (' + str(data[z]) + ',' + '\'' + names_list[z] + '\'' + ');\n')
-    f.write(str(data[z]) + ',' +names_list[z])
+    f.write(str(data[z]) + ',' +names_list[z]+'\n')
 
 f.close()
 
@@ -36,10 +36,10 @@ address_list_pc = []
 size_address_pc = 30
 for y in range(80000):
     address_list_pc.append(random_string(size_address_pc, chars_address_pc))
-f_pc = open('production.txt', 'w')
+f_pc = open('production.csv', 'w')
 for z in range(80000):
     # f_pc.write('INSERT INTO production_company values (' + str(data_pc[z]) + ',' + '\'' + names_list_pc[z] + '\''+  ',' + '\'' + address_list_pc[z] + '\'' + ');\n')
-    f_pc.write(str(data_pc[z])+ ','+names_list_pc[z]+','+address_list_pc[z])
+    f_pc.write(str(data_pc[z])+ ','+names_list_pc[z]+','+address_list_pc[z]+'\n')
 
 f_pc.close()
 
@@ -58,10 +58,10 @@ for y in range(1000000):
 pc_movie = np.random.permutation(np.append(np.random.randint(1, 501, 900000), np.random.randint(501, 80001, 100000)))
 year = np.random.randint(1900, 2001, 1000000)
 
-f = open('movie.txt', 'w')               # opening a file to write insert commands
+f = open('movie.csv', 'w')               # opening a file to write insert commands
 for z in range(1000000):
     # f.write('INSERT INTO movie values('+str(mid[z])+', \''+name_movie_list[z]+'\', '+ str(year[z]) + ', ' + str(imdb[z]) + ', ' + str(pc_movie[z]) + ');\n')
-    f.write(str(mid[z])+','+name_movie_list[z]+','+str(year[z])+','+str(imdb[z])+','+str(pc_movie[z]))
+    f.write(str(mid[z])+','+name_movie_list[z]+','+str(year[z])+','+str(imdb[z])+','+str(pc_movie[z])+'\n')
 
 f.close()
 
@@ -69,15 +69,15 @@ f.close()
 # random row generation for casting table
 mid_casting=np.random.permutation(1000000)
 aid_casting=np.random.permutation(np.append(np.random.randint(1, 10001, 3800000), np.random.randint(10001, 300001, 200000)))
-f_casting = open('casting.txt', 'w')
+f_casting = open('casting.csv', 'w')
 for z in range(1000000):
     # f_casting.write('INSERT INTO casting values('+str(mid_casting[z])+','+ str(aid_casting[z*1])+ ');\n')
     # f_casting.write('INSERT INTO casting values('+str(mid_casting[z])+','+ str(aid_casting[z*2])+ ');\n')
     # f_casting.write('INSERT INTO casting values('+str(mid_casting[z])+','+ str(aid_casting[z*3])+ ');\n')
     # f_casting.write('INSERT INTO casting values('+str(mid_casting[z])+','+ str(aid_casting[z*4])+ ');\n')
-    f_casting.write(str(mid_casting[z])+','+str(aid_casting[z*1]))
-    f_casting.write(str(mid_casting[z])+','+str(aid_casting[z*2]))
-    f_casting.write(str(mid_casting[z])+','+str(aid_casting[z*3]))
-    f_casting.write(str(mid_casting[z])+','+str(aid_casting[z*4]))
+    f_casting.write(str(mid_casting[z])+','+str(aid_casting[z*1])+'\n')
+    f_casting.write(str(mid_casting[z])+','+str(aid_casting[z*2])+'\n')
+    f_casting.write(str(mid_casting[z])+','+str(aid_casting[z*3])+'\n')
+    f_casting.write(str(mid_casting[z])+','+str(aid_casting[z*4])+'\n')
 
 f_casting.close()
